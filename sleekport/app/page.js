c753d3143ca22825a362b5f6ed541d4a28cbda7d
Kristian Worthy <ksworthy97@outlook.com>
2023-08-01 10:20:21 -0400

    committing nav changes

--- sleekport/app/page.js
+++ sleekport/app/page.js
@@ -1,10 +1,81 @@
-import Link from 'next/link';
-import Image from 'next/image'
-import mylogo from 'public/oksimonefr.png'
+"use client";
+import Navbar from '/app/navbar.jsx';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Navbar/>
+        );
+      }
+    
+  
+      {/*
+      <nav className="flex px-2 border-b md:shadow-lg items-center relative mt-10 rounded-full">
+        <div className="text-lg font-bold md:py-0 py-2">
+          <Image
+            src={mylogo}
+            alt="Kristian Logo"
+            width={150}
+            height={100}
+            priority
+          />
+        </div>
+        <ul className="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0">
+          <li>
+            <a href="#" className="flex md:inline-flex p-4 items-center hover:bg-gray-50">
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="flex md:inline-flex p-4 items-center hover:bg-gray-50">
+              <span>Products</span>
+            </a>
+          </li>
+          <li className="relative parent">
+            <a href="#" className="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
+              <span>Service</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24">
+                <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+              </svg>
+            </a>
+            <ul className="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:shadow-lg md:rounded-b">
+              <li>
+                <a href="#" className="flex px-4 py-3 hover:bg-gray-50">
+                  Web development
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex px-4 py-3 hover:bg-gray-50">
+                  Web Design
+                </a>
+              </li>
+              <li>
+                <a href="#" className="flex px-4 py-3 hover:bg-gray-50">
+                  Machine Learning
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" className="flex md:inline-flex p-4 items-center hover:bg-gray-50">
+              <span>About us</span>
+            </a>
+          </li>
+        </ul>
+        <div className="ml-auto md:hidden text-gray-500 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+            <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+          </svg>
+        </div>
+      </nav>
+      */
+  
+
+
+
+
+
+
+    /*<main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
        
         <nav>
@@ -23,8 +94,9 @@ export default function Home() {
           <Link href="/#">
             Contact
           </Link>
-        </li>
-        {/* Add more navigation items as needed */}
+        </li>*/
+        /* Add more navigation items as needed */
+        /*
       </ul>
     </nav>
           
@@ -48,6 +120,7 @@ export default function Home() {
 
 
       
-    </main>
-  )
-}
+    </main>*/}
+    
+
+
